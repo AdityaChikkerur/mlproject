fe79b7d3703f54902e42ea7f9301d110bcb24d02
AdityaChikkerur <adityachikkerur@gmail.com>
2026-05-04 21:30:40 +0530

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 #Error msg details to be displayed when an exception occurs
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
